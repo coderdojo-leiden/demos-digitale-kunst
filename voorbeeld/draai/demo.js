44f8c0a18e870e13f1b004283016e6c5c8fbb208
Jan Niestadt <jan.niestadt@ivdnt.org>
2023-06-29 18:25:10 +0200

    Update.

---
+++ voorbeeld/draai/demo.js
@@ -0,0 +1,19 @@
+function maakDemo()
+{ 
+
+// --------- HIER BEGINT JE DEMO --------------
+
+
+laag(Achtergrond)
+wijzig("kleur", "lichtblauw")
+
+laag(Vorm)
+wijzig("vorm", vorm.ster())
+wijzig("kleur", "zwart")
+laag(Draai)
+wijzig("hoek", getal.teller(20, 0, 360))
+
+
+// --------- HIER EINDIGT JE DEMO --------------
+
+}
---
+++ voorbeeld/grootte/demo.js
@@ -0,0 +1,19 @@
+function maakDemo()
+{ 
+
+// --------- HIER BEGINT JE DEMO --------------
+
+
+laag(Achtergrond)
+wijzig("kleur", "lichtblauw")
+
+laag(Vorm)
+wijzig("vorm", vorm.ster())
+wijzig("kleur", "zwart")
+laag(Grootte)
+wijzig("grootte", getal.golf(20, 50, 150))
+
+
+// --------- HIER EINDIGT JE DEMO --------------
+
+}
