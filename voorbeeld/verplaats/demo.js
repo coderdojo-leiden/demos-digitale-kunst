f028d3ed306efb61ca86437db4e147a7b8763312
Jan Niestadt <jan.niestadt@ivdnt.org>
2023-06-29 18:17:44 +0200

    Update.

---
+++ voorbeeld/verplaats/demo.js
@@ -0,0 +1,19 @@
+function maakDemo()
+{ 
+
+// --------- HIER BEGINT JE DEMO --------------
+
+
+laag(Achtergrond)
+wijzig("kleur", "lichtblauw")
+
+laag(Vorm)
+wijzig("vorm", vorm.vierkant())
+wijzig("kleur", kleur.regenboog(100, 50))
+laag(Verplaats)
+wijzig("pad", pad.lissa())
+
+
+// --------- HIER EINDIGT JE DEMO --------------
+
+}
